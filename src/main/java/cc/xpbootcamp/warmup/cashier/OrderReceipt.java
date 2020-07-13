cf90e08db21c61008257d55ca63fe22651fa2f5b
yusong <yusong.wang@thoughtworks.com>
2020-07-13 22:47:09 +0800

    initial solution

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -6,8 +6,15 @@ package cc.xpbootcamp.warmup.cashier;
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
  *
+ * 思路：
+ * - 首先这个方法有很多的常量可以抽出来便于维护
+ * - 其中计算税的部分可以单独抽一个方法
+ * - 打印每个LineItem也可以抽出来
+ * - 变量名字有些迷惑
  */
 public class OrderReceipt {
+
+    public static final double TAX_RATE = .10;
     private Order order;
 
     public OrderReceipt(Order order) {
@@ -25,24 +32,22 @@ public class OrderReceipt {
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 //        output.append(order.getCustomerLoyaltyNumber());
+        appendTax(output);
 
+        for (LineItem lineItem : order.getLineItems()) {
+            appendBasicInfo(output, lineItem);
+        }
+        return output.toString();
+    }
+
+    private void appendTax(StringBuilder output) {
         // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
-
             // calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
+            double salesTax = lineItem.totalAmount() * TAX_RATE;
             totSalesTx += salesTax;
-
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
         }
@@ -52,6 +57,16 @@ public class OrderReceipt {
 
         // print total amount
         output.append("Total Amount").append('\t').append(tot);
-        return output.toString();
+    }
+
+    private void appendBasicInfo(StringBuilder output, LineItem lineItem) {
+        output.append(lineItem.getDescription());
+        output.append('\t');
+        output.append(lineItem.getPrice());
+        output.append('\t');
+        output.append(lineItem.getQuantity());
+        output.append('\t');
+        output.append(lineItem.totalAmount());
+        output.append('\n');
     }
 }
